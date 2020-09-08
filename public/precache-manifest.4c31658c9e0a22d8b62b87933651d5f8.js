ba4c383456d7862d688156cc74a12f8e9fd1f000
Natalia López <natalialopezyeste@gmail.com>
2020-09-08 20:30:08 +0200

    initial commit to heroku

---
+++ public/precache-manifest.4c31658c9e0a22d8b62b87933651d5f8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbe58cdd53f487cfb14e90ee68d3b2f4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4f062fdfbddb37ed4377",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "2cce27dc16bf38a83a14",
+    "url": "/static/css/main.1c30b8f9.chunk.css"
+  },
+  {
+    "revision": "4f062fdfbddb37ed4377",
+    "url": "/static/js/2.d5002d9c.chunk.js"
+  },
+  {
+    "revision": "2badc1b209c5a040c707dcd720b10f8a",
+    "url": "/static/js/2.d5002d9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2cce27dc16bf38a83a14",
+    "url": "/static/js/main.d159f869.chunk.js"
+  },
+  {
+    "revision": "ec80e7a4cfaf4ff54c6e",
+    "url": "/static/js/runtime-main.9eed2229.js"
+  }
+]);
