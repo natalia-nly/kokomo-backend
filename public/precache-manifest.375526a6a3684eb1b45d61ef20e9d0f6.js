7d15dd720982ab8c3815fe6083a637e5d467dcdc
Natalia López <natalialopezyeste@gmail.com>
2020-09-12 13:21:42 +0200

    new build

---
+++ public/precache-manifest.375526a6a3684eb1b45d61ef20e9d0f6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc5d5892dc83ceb6c15ff89493b3ee88",
+    "url": "/index.html"
+  },
+  {
+    "revision": "627ea3b1995ef38dc83b",
+    "url": "/static/css/2.b9fbc4d1.chunk.css"
+  },
+  {
+    "revision": "581b3df2da2fa26538b3",
+    "url": "/static/css/main.ab099198.chunk.css"
+  },
+  {
+    "revision": "627ea3b1995ef38dc83b",
+    "url": "/static/js/2.56dd7ed9.chunk.js"
+  },
+  {
+    "revision": "2badc1b209c5a040c707dcd720b10f8a",
+    "url": "/static/js/2.56dd7ed9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "581b3df2da2fa26538b3",
+    "url": "/static/js/main.9766e9a3.chunk.js"
+  },
+  {
+    "revision": "ec80e7a4cfaf4ff54c6e",
+    "url": "/static/js/runtime-main.9eed2229.js"
+  }
+]);
