150956cebdc11da1f77e5a765b3216b8c4cb005d
Claudi Sanchez <claudi.sanchezserrano@gmail.com>
2020-09-14 18:46:32 +0200

    delete message

--- routes/index.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router  = express.Router();
-const indexController = require('../controllers/index.controllers');
-
-// GET home page
-router.get('/', indexController.allProperties);
-
-module.exports = router;
