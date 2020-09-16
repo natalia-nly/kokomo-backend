d68b8e996b39d2e159cf260640d910d3e3929049
Claudi Sanchez <claudi.sanchezserrano@gmail.com>
2020-09-16 12:58:56 +0200

    readme backend

--- routes/profile.routes.js
+++ routes/profile.routes.js
@@ -4,8 +4,6 @@ const profileController = require('../controllers/profile.controllers');
 
 // GET de la ruta profile del customer
 router.get("/profile", profileController.profile);
-//Editar el profile
-router.get("/profile/edit", profileController.profileEdit);
 //Editar la contraseña
 router.post("/profile/editPassword", profileController.profilePasswordChange);
 //Editar el teléfono
