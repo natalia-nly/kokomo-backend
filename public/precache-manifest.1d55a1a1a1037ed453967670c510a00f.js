a1a3706e7027b84869576964bab332ee89b88236
Natalia López <natalialopezyeste@gmail.com>
2020-09-19 11:05:52 +0200

    Google Analytics y primer styled components

--- public/precache-manifest.1d55a1a1a1037ed453967670c510a00f.js
+++ public/precache-manifest.1d55a1a1a1037ed453967670c510a00f.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "6ac4bfa128e2f3e936f7a8c05d335720",
+    "revision": "e6d557ea1e8dc806c8fd790ed6284679",
     "url": "/index.html"
   },
   {
-    "revision": "13247c698749c727d4ae",
+    "revision": "9bddfce1e8298b9eaade",
     "url": "/static/css/2.b9fbc4d1.chunk.css"
   },
   {
-    "revision": "8a6d764742f497554245",
+    "revision": "90077a787a16080372f5",
     "url": "/static/css/main.bac6cb61.chunk.css"
   },
   {
-    "revision": "13247c698749c727d4ae",
-    "url": "/static/js/2.d45fe35f.chunk.js"
+    "revision": "9bddfce1e8298b9eaade",
+    "url": "/static/js/2.21122e87.chunk.js"
   },
   {
     "revision": "2badc1b209c5a040c707dcd720b10f8a",
-    "url": "/static/js/2.d45fe35f.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.21122e87.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "8a6d764742f497554245",
-    "url": "/static/js/main.a4461f87.chunk.js"
+    "revision": "90077a787a16080372f5",
+    "url": "/static/js/main.98cc580f.chunk.js"
   },
   {
     "revision": "ec80e7a4cfaf4ff54c6e",
