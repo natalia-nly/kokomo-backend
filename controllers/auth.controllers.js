21c4eb1fd7a99ace086fba2c184514501ec3a66f
natalia-nly <natalialopezyeste@gmail.com>
2021-02-02 22:49:59 +0100

    Cambiando modelos y pasando a async

--- controllers/auth.controllers.js
+++ controllers/auth.controllers.js
@@ -1,181 +1,177 @@
-const Customer = require("../models/customer.model");
-const mongoose = require('mongoose');
-const bcrypt = require("bcryptjs");
-const saltRounds = 10;
-const passport = require("passport");
-
+const Customer = require('../models/customer.model')
+const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
+const saltRounds = 10
+const passport = require('passport')
 
 //Creación de Avatar Random
 const randomAvatar = () => {
-  const avatarArr = [
-    '/images/avatar1.jpg',
-    '/images/avatar2.jpg',
-    '/images/avatar3.jpg',
-    '/images/avatar4.jpg',
-    '/images/avatar5.jpg'
-  ];
-  return avatarArr[Math.floor(Math.random() * 5)];
-};
+   const avatarArr = [
+      '/images/avatar1.jpg',
+      '/images/avatar2.jpg',
+      '/images/avatar3.jpg',
+      '/images/avatar4.jpg',
+      '/images/avatar5.jpg'
+   ]
+   return avatarArr[Math.floor(Math.random() * 5)]
+}
 //Registrar el customer en BBDD
 exports.registerCustomer = (req, res, next) => {
-  console.log(req.body)
-  const {
-    username,
-    telNumber,
-    email,
-    password
-  } = req.body;
-  if (!username || !telNumber || !email || !password) {
-    res.status(400).json({ message: 'Necesitas completar todos los campos para crear tu cuenta' });
-    return;
-  }
-  const regex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
-  if (!regex.test(password)) {
-    res.status(500).json({ message: 'La contraseña debe tener al menos 6 caracteres y debe contener, por lo menos, una letra minúscula, una mayúscula y un número.' });
-    return;
-  }
-  const regexEmail = /\S+@\S+\.\S+/;
-  if (!regexEmail.test(email)) {
-    res.status(500).json({ message: 'Por favor, pon una dirección de correo válida' });
-    return;
-  }
-  bcrypt
-    .genSalt(saltRounds)
-    .then((salt) => bcrypt.hash(password, salt))
-    .then((hashedPassword) => {
-      return Customer.create({
-        username,
-        email,
-        telNumber: telNumber,
-        avatar: randomAvatar(),
-        passwordHash: hashedPassword,
-      });
-    })
-    .then((userFromDB) => {
-      console.log("Customer creado: ", userFromDB.username);
-      req.session.currentUser = userFromDB;
-      console.log(userFromDB);
-      res.status(200).json(userFromDB);
-    })
-    .catch((error) => {
-      if (error instanceof mongoose.Error.ValidationError) {
-        res.status(400).json({ message: error.message });
-      } else if (error.code === 11000) {
-        res.status(400).json({ message: 'El username ya existe...' });
-      } else {
-        next(error);
-      }
-    });
-};
+   console.log(req.body)
+   const { username, telNumber, email, password } = req.body
+   if (!username || !email || !password) {
+      return res.status(400).json({
+         message: 'Necesitas completar todos los campos para crear tu cuenta'
+      })
+   }
+   const regex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/
+   if (!regex.test(password)) {
+      return res.status(500).json({
+         message:
+            'La contraseña debe tener al menos 6 caracteres y debe contener, por lo menos, una letra minúscula, una mayúscula y un número.'
+      })
+   }
+   const regexEmail = /\S+@\S+\.\S+/
+   if (!regexEmail.test(email)) {
+      return res.status(500).json({
+         message: 'Por favor, pon una dirección de correo válida'
+      })
+   }
+
+   bcrypt
+      .genSalt(saltRounds)
+      .then((salt) => bcrypt.hash(password, salt))
+      .then((hashedPassword) => {
+         return Customer.create({
+            username,
+            email,
+            telNumber,
+            avatar: randomAvatar(),
+            passwordHash: hashedPassword
+         })
+      })
+      .then((userFromDB) => {
+         req.session.currentUser = userFromDB
+         console.log(userFromDB)
+         res.status(200).json(userFromDB)
+      })
+      .catch((error) => {
+         console.log(error)
+      })
+}
 //Registrar el customer como Owner
 exports.registerOwner = (req, res, next) => {
-  const {
-    username,
-    telNumber,
-    email,
-    password
-  } = req.body;
-  if (!username || !telNumber || !email || !password) {
-    res.status(400).json({ message: 'Necesitas completar todos los campos para crear tu cuenta' });
-    return;
-  }
-  const regex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
-  if (!regex.test(password)) {
-    res.status(500).json({ message: 'La contraseña debe tener al menos 6 caracteres y debe contener, por lo menos, una letra minúscula, una mayúscula y un número.' });
-    return;
-  }
-  const regexEmail = /\S+@\S+\.\S+/;
-  if (!regexEmail.test(email)) {
-    res.status(500).json({ message: 'Por favor, pon una dirección de correo válida' });
-    return;
-  }
-  bcrypt
-    .genSalt(saltRounds)
-    .then((salt) => bcrypt.hash(password, salt))
-    .then((hashedPassword) => {
-      return Customer.create({
-        username,
-        email,
-        telNumber: telNumber,
-        avatar: randomAvatar(),
-        passwordHash: hashedPassword,
-        owner: true,
-      });
-    })
-    .then((userFromDB) => {
-      console.log("Customer creado: ", userFromDB.username);
-      req.session.currentUser = userFromDB;
-      console.log(userFromDB);
-      res.status(200).json(userFromDB);
-    })
-    .catch((error) => {
-      if (error instanceof mongoose.Error.ValidationError) {
-        res.status(400).json({ message: error.message });
-      } else if (error.code === 11000) {
-        res.status(400).json({ message: 'El username ya existe...' });
-      } else {
-        next(error);
-      }
-    });
-};
-
+   const { username, telNumber, email, password } = req.body
+   if (!username || !telNumber || !email || !password) {
+      res.status(400).json({
+         message: 'Necesitas completar todos los campos para crear tu cuenta'
+      })
+      return
+   }
+   const regex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/
+   if (!regex.test(password)) {
+      res.status(500).json({
+         message:
+            'La contraseña debe tener al menos 6 caracteres y debe contener, por lo menos, una letra minúscula, una mayúscula y un número.'
+      })
+      return
+   }
+   const regexEmail = /\S+@\S+\.\S+/
+   if (!regexEmail.test(email)) {
+      res.status(500).json({
+         message: 'Por favor, pon una dirección de correo válida'
+      })
+      return
+   }
+   bcrypt
+      .genSalt(saltRounds)
+      .then((salt) => bcrypt.hash(password, salt))
+      .then((hashedPassword) => {
+         return Customer.create({
+            username,
+            email,
+            telNumber: telNumber,
+            avatar: randomAvatar(),
+            passwordHash: hashedPassword,
+            owner: true
+         })
+      })
+      .then((userFromDB) => {
+         console.log('Customer creado: ', userFromDB.username)
+         req.session.currentUser = userFromDB
+         console.log(userFromDB)
+         res.status(200).json(userFromDB)
+      })
+      .catch((error) => {
+         if (error instanceof mongoose.Error.ValidationError) {
+            res.status(400).json({ message: error.message })
+         } else if (error.code === 11000) {
+            res.status(400).json({ message: 'El username ya existe...' })
+         } else {
+            next(error)
+         }
+      })
+}
 
 //Logearse
 exports.login = (req, res, next) => {
-  console.log(req.body)
-  passport.authenticate('local', (err, theUser, failureDetails) => {
+   console.log(req.body)
+   passport.authenticate('local', (err, theUser, failureDetails) => {
       if (err) {
-          res.status(500).json({ message: 'Something went wrong authenticating user' });
-          return;
+         res.status(500).json({
+            message: 'Something went wrong authenticating user'
+         })
+         return
       }
 
       if (!theUser) {
-          // "failureDetails" contains the error messages
-          // from our logic in "LocalStrategy" { message: '...' }.
-          res.status(401).json(failureDetails);
-          return;
+         // "failureDetails" contains the error messages
+         // from our logic in "LocalStrategy" { message: '...' }.
+         res.status(401).json(failureDetails)
+         return
       }
 
       // save user in session
       req.login(theUser, (err) => {
-          if (err) {
-              res.status(500).json({ message: 'Session save went bad.' });
-              return;
-          }
-          // We are now logged in (that's why we can also send req.user)
-          res.status(200).json(theUser);
-      });
-  })(req, res, next);
-};
+         if (err) {
+            res.status(500).json({ message: 'Session save went bad.' })
+            return
+         }
+         // We are now logged in (that's why we can also send req.user)
+         res.status(200).json(theUser)
+      })
+   })(req, res, next)
+}
 
 //LogOut
 exports.logout = (req, res) => {
-  req.logout();
-  res.status(200).json({ message: 'Log out success!' });
-};
+   req.logout()
+   res.status(200).json({ message: 'Log out success!' })
+}
 
 //LoggedIn
 exports.loggedIn = (req, res) => {
-  // req.isAuthenticated() is defined by passport
-  if (req.isAuthenticated()) {
-    console.log('result isAuthenticated',req.user)
-    res.status(200).json(req.user);
-    return;
-  }
-  res.status(403).json({ message: 'Unauthorized' });
-};
+   // req.isAuthenticated() is defined by passport
+   if (req.isAuthenticated()) {
+      console.log('result isAuthenticated', req.user)
+      res.status(200).json(req.user)
+      return
+   }
+   res.status(403).json({ message: 'Unauthorized' })
+}
 
 //Info user
 exports.infoUser = (req, res) => {
-  console.log('user_id: ',req.user._id)
-  // req.isAuthenticated() is defined by passport
+   console.log('user_id: ', req.user._id)
+   // req.isAuthenticated() is defined by passport
 
-  Customer.findById(req.user._id).populate('ownProperties').populate('bookings')
-  .then(user => {
-    console.log(user)
-    res.status(200).json(user);
-    return;
-    
-  }) .catch(error => next(error));
-  
-};
+   Customer.findById(req.user._id)
+      .populate('ownProperties')
+      .populate('bookings')
+      .then((user) => {
+         console.log(user)
+         res.status(200).json(user)
+         return
+      })
+      .catch((error) => next(error))
+}
