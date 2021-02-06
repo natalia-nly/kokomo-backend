13b98d03ac3de0ed6972afdc1e5b4a10329d6e95
natalia-nly <natalialopezyeste@gmail.com>
2021-02-06 22:05:40 +0100

    Build

---
+++ public/precache-manifest.42fd886bde5ba9ce945ee9ee770b00a0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5657ab7b432149eb4508b172ba24a542",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7dddf00682198b407645",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "5bae18fbe51b5802f7c3",
+    "url": "/static/css/main.69ac062b.chunk.css"
+  },
+  {
+    "revision": "7dddf00682198b407645",
+    "url": "/static/js/2.472150f0.chunk.js"
+  },
+  {
+    "revision": "2badc1b209c5a040c707dcd720b10f8a",
+    "url": "/static/js/2.472150f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5bae18fbe51b5802f7c3",
+    "url": "/static/js/main.c6cc5271.chunk.js"
+  },
+  {
+    "revision": "ec80e7a4cfaf4ff54c6e",
+    "url": "/static/js/runtime-main.9eed2229.js"
+  }
+]);
